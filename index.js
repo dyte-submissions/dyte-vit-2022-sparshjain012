9da4f300f61749a4f226bf29f38eb25104488d3d
Sparsh Jain <sparsh.jain@quesba.com>
2022-06-01 19:40:52 +0530

    Dyte Submission

--- index.js
+++ index.js
@@ -4,66 +4,33 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
 const files = require('./lib/files');
-const github = require('./lib/github');
+const github = require('./lib/compare_version');
 const repo = require('./lib/repo');
 
 clear();
 
 console.log(
   chalk.yellow(
-    figlet.textSync('Ginit', { horizontalLayout: 'full' })
+    figlet.textSync('Dyte Submission', { horizontalLayout: 'full' })
   )
 );
 
-if (files.directoryExists('.git')) {
-  console.log(chalk.red('Already a Git repository!'));
-  process.exit();
-}
 
-const getGithubToken = async () => {
-  // Fetch token from config store
-  let token = github.getStoredGithubToken();
-  if(token) {
-    return token;
-  }
-
-  // No token found, use credentials to access GitHub account
-  token = await github.getPersonalAccesToken();
-
-  return token;
-};
-
+const inquirer  = require('./lib/inquirer');
+const filedata =require('./lib/files');
 const run = async () => {
-  try {
-    // Retrieve & Set Authentication Token
-    const token = await getGithubToken();
-    github.githubAuth(token);
-
-    // Create remote repository
-    const url = await repo.createRemoteRepo();
-
-    // Create .gitignore file
-    await repo.createGitignore();
-
-    // Set up local repository and push to remote
-    await repo.setupRepo(url);
-
-    console.log(chalk.green('All done!'));
-  } catch(err) {
-      if (err) {
-        switch (err.status) {
-          case 401:
-            console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token.'));
-            break;
-          case 422:
-            console.log(chalk.red('There is already a remote repository or token with the same name'));
-            break;
-          default:
-            console.log(chalk.red(err));
-        }
-      }
-  }
+  const credentials = await inquirer.askGithubCredentials();
+  //read csv file name from user
+  //console.log(credentials.FileName);
+  var check_dependency_name=credentials.Dependencyname;
+  var Version=credentials.Versionname;
+  //read csv file
+  const filedetails=await filedata.getfiledetails(credentials.FileName,check_dependency_name,Version);
+  //console.log(credentials.Dependencyname);
 };
-
 run();
